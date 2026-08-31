7d91d26f2572ba6b63a0d5cfef09eda7fe73417b
Madhavan Patel <madhavanpatel19@gmail.com>
2026-08-31 16:29:53 +0530

    db file ignore

--- admin_area/includes/db.php
+++ admin_area/includes/db.php
@@ -1,10 +1,10 @@
 <?php
-
+$config = require dirname(__DIR__) . '/../environment.php';
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 date_default_timezone_set('Asia/Kolkata');
 /** @var mysqli $con */
-$con = mysqli_connect("localhost", "root", "", "cadlete_crm", 3306);
+$con = mysqli_connect($config['DB_HOST'], $config['DB_USER'], $config['DB_PASS'], $config['DB_NAME'], $config['DB_PORT']);
 
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
